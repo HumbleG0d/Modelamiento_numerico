128d30294cdbeea1bd6eb52e9b8fe044c391ee3e
HumbleG0d <sergiouni2000@gmail.com>
2023-09-28 14:57:22 -0500

    chnage matrix A and b

--- Gauss_Jordan/gauss_jordan.py
+++ Gauss_Jordan/gauss_jordan.py
@@ -1,42 +1,49 @@
 import numpy as np
 import sys
 
-def gauss_jordan(A,b):
+
+def gauss_jordan(A, b):
     M = A.copy()
     n = len(b)
     x = np.zeros(n)
 
     for i in range(len(b)):
-        M[i].insert(len(b) + 1 , b[i][0])
-    
+        M[i].insert(len(b) + 1, b[i][0])
+
     print("Matriz Aumentada: ")
-    print('\n'.join([''.join(['{:6}'.format(item) for item in row]) for row in M]))
+    print("\n".join(["".join(["{:6}".format(item) for item in row]) for row in M]))
 
     for i in range(n):
         if M[i][i] == 0.0:
-            sys.exit('Division por 0 detectada')
+            sys.exit("Division por 0 detectada")
 
         for j in range(n):
             if i != j:
                 ratio = M[j][i] / M[i][i]
 
-                for k in range(n+1):
+                for k in range(n + 1):
                     M[j][k] = M[j][k] - ratio * M[i][k]
 
     print("\nMatriz Triangular:")
-    print('\n'.join([''.join(['{:10.4f}'.format(item) for item in row]) for row in M]))
+    print("\n".join(["".join(["{:10.4f}".format(item) for item in row]) for row in M]))
 
     for i in range(n):
         x[i] = M[i][n] / M[i][i]
     print("\nMatriz solución: ")
     for i in range(n):
-        print('X%d = %0.2f' % (i , x[i]) , end = '\t')
+        print(
+            "X%d = %0.6f" % (i, x[i]), end="\t"
+        )  # 0.6f indica la cantidad de decimales
 
-A = [[1.5 , 2 ,3] ,
-     [1 , 1.5 , 2],
-     [1 , 1 ,1],]
-b = [[250] ,
-     [175],
-     [140],]
 
-gauss_jordan(A,b)
+A = [
+    [0.100 * 10**-3, 0.100 * 10**1],
+    [0.100 * 10**1, 0.100 * 10**1],
+]
+b = [
+    [0.100 * 10**1],
+    [0.300 * 10**1],
+]
+
+
+gauss_jordan(A, b)
