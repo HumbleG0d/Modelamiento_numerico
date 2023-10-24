3affa20eeac56c833df216b4a00c6e804b7a1035
HumbleG0d <sergiouni2000@gmail.com>
2023-10-24 17:15:04 -0500

    change the code to calculate matrix decomposition

--- LU/Crout.py
+++ LU/Crout.py
@@ -1,32 +1,75 @@
 import numpy as np
 
 
-def lu_crout(A):
-    n = A.shape[0]
-    L = np.zeros((n, n))
-    U = np.eye(n)
+def lu_crout(A, b):
+    # Verficamos que la determinante de cada matriz esquina sea diferente de cero
+    minors_nonzero = all(
+        np.linalg.det(A[: i + 1, : i + 1]) != 0 for i in range(A.shape[0])
+    )
 
-    for i in range(n):
-        # Calcular L
-        for j in range(i, n):
-            L[j, i] = A[j, i] - np.dot(L[j, :i], U[:i, i])
+    if not minors_nonzero:
+        print("La matriz no se puede descomponer por LU")
+    else:
+        m, n = A.shape
+        L = np.zeros((n, n))
+        U = np.zeros((n, n))
+        s1 = 0
+        s2 = 0
+        for i in range(n):
+            L[i][0] = A[i][0]
+            U[i][i] = 1
+            print("U", i + 1, i + 1, "=", U[i][i])
+            print("L", i + 1, 1, "=", L[i][0])
 
-        # Calcular U
-        for j in range(i + 1, n):
-            U[i, j] = (A[i, j] - np.dot(L[i, :i], U[:i, j])) / L[i, i]
+        for j in range(1, n):
+            U[0][j] = A[0][j] / L[0][0]
+            print("U", 1, j + 1, "=", U[0][j])
+        for k in range(1, n):
+            for i in range(k, n):
+                for r in range(k):
+                    s1 += L[i][r] * U[r][k]
+                L[i][k] = A[i][k] - s1
+                print("L", i + 1, k + 1, "=", L[i][k])
+                s1 = 0
+            for j in range(k + 1, n):
+                for r in range(k):
+                    s2 += L[k][r] * U[r][j]
+                U[k][j] = (A[k][j] - s2) / L[k][k]
+                print("U", k + 1, j + 1, "=", U[k][j])
+                s2 = 0
+        print("\nMatriz L:")
+        print(L)
+        print("\nMatriz U:")
+        print(U)
 
-    return L, U
+        """
+      Inicialmenete teniamos el sistema Ax = b
+      Ahora tenemos el sistema LUx = b.
+      Sea Ux = y , asi tendremos Ly = b
+      Primero resolvamos Ly = b.
+      Luego resolvemos Ux = y.
+    """
+        # Resolviendo Ly = b.Para ello aplicamos sustitucion progresiva
+        print("\nCalculando la matriz ´y´ por sustitucion progresiva.")
+        y = np.zeros(n)
+        for i in range(n):
+            y[i] = (b[i] - np.dot(L[i, :i], y[:i])) / L[i, i]
+
+        print("Matriz y:")
+        print(y)
+
+    # Finlmente resolvemos Ux = y. Para ello aplicamos sustitucion regresiva
+    print("\nCalculando la matriz ´x´ por sustitucion regresiva.")
+    x = np.zeros(n)
+    for i in range(n - 1, -1, -1):
+        x[i] = (y[i] - np.dot(U[i, i + 1 :], x[i + 1 :])) / U[i, i]
+
+    print("Matriz solucion:")
+    print(x)
 
 
-A = np.array([[1, 2, 20], [1, -2, 0], [1, 1, 1]], dtype=float)
+A = np.array([[10, -3, 6], [1, -8, -2], [-2, 4, 9]], dtype=float)
 
-L, U = lu_crout(A)
+b = np.array([[25], [-9], [-50]], dtype=float)
 
-print("Matriz L (triangular inferior):")
-print(L)
-
-print("\nMatriz U (triangular superior):")
-print(U)
-
-print("\nVerificación: A == L * U")
-print(np.allclose(A, L @ U))
+lu_crout(A, b)
